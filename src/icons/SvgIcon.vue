014066608866f9c5870cb2083620f15cbd2afa5f
1319pkq <61814043+1319pkq@users.noreply.github.com>
2020-05-22 16:13:45 +0800

    更新

--- src/icons/SvgIcon.vue
+++ src/icons/SvgIcon.vue
@@ -4,10 +4,8 @@
     </svg>
 </template>
 <script>
-import { ref, computed, onMounted } from '@vue/composition-api';
 export default {
     name: "svgIcon",
-    // props: ['iconClass', 'className'],
     props: {
         // 分页
         iconClass: {
@@ -18,28 +16,19 @@ export default {
             type: String,
             default: ''
         }
-    },
-    setup(props){
-        const msg = ref("手把手撸码的说法的前端");
-        const number = ref(1);
-
-        // 计算属性，得出最终结果，并返回
-        const iconName = computed(() => `#icon-${props.iconClass}`)
-
-        const svgClass = computed(() => {
-            if(props.className) {
-                return `svg-icon ${props.className}`
-            }else{
-                return `svg-icon`
-            }
-        })
-
-        return {
-            msg,
-            iconName,
-            svgClass
-        }
-    }
+    },   
+		computed: {
+			iconName() {
+				return `#icon-${this.iconClass}`
+			},
+			svgClass() {
+				if(this.className) {
+				    return `svg-icon ${this.className}`
+				}else{
+				    return `svg-icon`
+				}
+			}
+		},
 }
 </script>
 <style lang="scss" scoped>
